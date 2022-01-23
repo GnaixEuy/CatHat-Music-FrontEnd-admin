7f937d289252ea5e3067d626acd10a5d618bc3a2
GnaixEuy <sgghcj@live.com>
2022-01-23 19:04:42 +0800

    :heavy_plus_sign: add axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,11 +4,18 @@ import {ref} from 'vue'
 const count = ref(0)
 const message = ref("猫猫头音乐后台")
 
+const hi = () => {
+  message.value = '已请求'
+}
+
 </script>
 
 <template>
   <h1>{{ message }}</h1>
   <q-btn color="primary" label="Primary" @click="count++">点击次数: {{ count }}</q-btn>
+  <br>
+  <br>
+  <q-btn color="primary" label="Primary" @click="hi">请求接口</q-btn>
 </template>
 
 <style scoped>
