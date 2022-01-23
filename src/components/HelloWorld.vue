ccab4759fb6122168dfc4e175a58ddf807ba69dc
GnaixEuy <sgghcj@live.com>
2022-01-23 18:59:14 +0800

    :heavy_plus_sign: import quasar config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,14 @@
 <script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
+import {ref} from 'vue'
 
 const count = ref(0)
+const message = ref("猫猫头音乐后台")
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h1>{{ message }}</h1>
+  <q-btn color="primary" label="Primary" @click="count++">点击次数: {{ count }}</q-btn>
 </template>
 
 <style scoped>
